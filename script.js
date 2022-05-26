16b09c09d48aaf40d66d5f74999c794fd2a5becf
Tungbui <jokerdt@gmail.com>
2022-05-26 20:56:47 +0700

    add button and query selector

--- script.js
+++ script.js
@@ -1,61 +1,104 @@
 let playerScore = 0;
 let computerScore = 0;
-let computerSelection;
-let playerSelection;
+let roundWinner = '';
 
-function computerPlay() {
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+
+const scoreMes = document.getElementById('scoreMes');
+const playerScoreMes = document.getElementById('playerScoreMes');
+const computerScoreMes = document.getElementById('computerScoreMes');
+
+rockBtn.addEventListener('click', () => userClick('rock'));
+paperBtn.addEventListener('click', () => userClick('paper'));
+scissorsBtn.addEventListener('click', () => userClick('scissors'));
+
+function userClick(playerSelection) {   
+    checkWinnder();
+    const computerSelection = computerChoice();
+    playRound(playerSelection,computerSelection);
+    updateChoices(playerSelection,computerSelection);
+    updateScore();
+}
+
+function computerChoice() {
     let computerRandom = Math.floor(Math.random()*3);
     switch (computerRandom) {
-    case 0:
-        return 'rock'
-    case 1:
-        return 'paper'
-    case 2:
-        return 'scissors'
+        case 0:
+            return 'rock'
+        case 1:
+            return 'paper'
+        case 2:
+            return 'scissors'
     }
 }
 
-function userInput() {
-    let playerSelection = prompt('rock,paper,scissor?','rock')
-    if ( (playerSelection.toLowerCase() === 'rock') || (userInput.toLowerCase() === 'paper') || (userInput.toLowerCase() === 'scissors') ) {
-    playRound(playerSelection,computerSelection);
+function checkWinnder() {
+    if (playerScore === 5 || computerScore === 5) {
+        declareWinner();
+    }
+}
+
+function declareWinner() {
+    if (playerScore === 5) {
+        
+    }
+}
+
+function playRound(playerSelection,computerSelection) {    
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection = 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        roundWinner = 'player';
+        ++playerScore;
     } else {
-    alert('Invalid Choice!');
-    userInput();
-    }
+        roundWinner = 'computer';
+        ++computerScore;
+    } return roundWinner;
 }
 
-function playRound(playerSelection,computerSelection) {
-    computerSelection = computerPlay();
-    if ( (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper') ) {
-    playerScore++;
-    alert('Player Win!');
-    console.log('Player Choice:'+playerSelection+'\nComputer Choice: '+computerSelection+'\nPlayer Win!\nScore: '+playerScore+' - '+computerScore);
-    } else if ( (computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'paper' && playerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper') ) {
-    computerScore++;
-    alert('Computer Win!');
-    console.log('Player Choice:'+playerSelection+'\nComputer Choice: '+computerSelection+'\nComputer Win!\nScore: ' + playerScore + ' - ' + computerScore);
-    } else if (playerSelection === computerSelection) {
-    alert('Game Tie');
-    console.log('Player Choice:'+playerSelection+'\nComputer Choice: '+computerSelection+'\nGame Tie!\nScore: ' + playerScore + ' - ' + computerScore);
-    } else {
-    console.log('Wrong Input');
+function updateChoices(playerSelection, computerSelection) {
+    switch (playerSelection) {
+      case 'rock':
+        playerSign.textContent = '✊'
+        break
+      case 'paper':
+        playerSign.textContent = '✋'
+        break
+      case 'scissors':
+        playerSign.textContent = '✌'
+        break
     }
-}
-
-function game() {
-    for (let i = 0; playerScore < 5 && computerScore < 5; i ++) {
-    userInput();        
+  
+    switch (computerSelection) {
+      case 'rock':
+        computerSign.textContent = '✊'
+        break
+      case 'paper':
+        computerSign.textContent = '✋'
+        break
+      case 'scissors':
+        computerSign.textContent = '✌'
+        break
     }
-    console.log('Final score: ' + playerScore + ' - ' + computerScore);
-    if ( playerScore == computerScore ) {
-    console.log('Game Tie!');
-    } else if ( playerScore > computerScore ) {
-    console.log('Player Win!');
-    } else {
-    console.log('Computer Win!');
-    }
-}
+  }  
 
-game();
+  function updateScore() {
+      if (roundWinner === 'tie') {
+        scoreMes.textContent = 'Hòa! Nại nào!';
+      } 
+      if (roundWinner === 'player') {
+          scoreMes.textContent = 'Win! Tiếp đi pạn!';
+      } 
+      if (roundWinner === 'computer') {
+          scoreMes.textContent = 'Thua rồi! Cố nên pạn!';
+      }
 
+      playerScoreMes.textContent = `Pạn: ${playerScore}`;
+      computerScoreMes.textContent = `Vida: ${computerScore}`;
+  }
