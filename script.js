86615f2f311967a757d0b831e1a8bf1b0492ee57
Tungbui <jokerdt@gmail.com>
2022-05-31 20:41:05 +0700

    add end game message and restart button

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let playerScore = 0;
 let computerScore = 0;
 let roundWinner = '';
+let round = 0;
 
 const rockBtn = document.getElementById('rockBtn');
 const paperBtn = document.getElementById('paperBtn');
 const scissorsBtn = document.getElementById('scissorsBtn');
+const playerRestartBtn = document.getElementById('playerRestartBtn');
+const computerRestartBtn = document.getElementById('computerRestartBtn');
+
+const playerWin = document.getElementById('playerWin');
+const computerWin = document.getElementById('computerWin');
+
+const playerSign = document.getElementById('playerSign');
+const computerSign = document.getElementById('computerSign');
+const roundMes = document.getElementById('roundMes');
 
 const scoreMes = document.getElementById('scoreMes');
 const playerScoreMes = document.getElementById('playerScoreMes');
@@ -13,13 +23,32 @@ const computerScoreMes = document.getElementById('computerScoreMes');
 rockBtn.addEventListener('click', () => userClick('rock'));
 paperBtn.addEventListener('click', () => userClick('paper'));
 scissorsBtn.addEventListener('click', () => userClick('scissors'));
+playerRestartBtn.addEventListener('click', () => restartGame());
+computerRestartBtn.addEventListener('click', () => restartGame());
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 0;
+    roundMes.textContent = 'Ván: 0';
+    scoreMes.textContent = 'Pạn nhớ là cứ bình tĩnh mà xiên ló!';
+    playerSign.textContent = '❔';
+    computerSign.textContent = '❔';
+    playerScoreMes.textContent = 'Pạn: 0';
+    computerScoreMes.textContent = 'Vida: 0';
+    document.getElementById("playerWin").style.display = "none";
+    document.getElementById("computerWin").style.display = "none";
+}
 
 function userClick(playerSelection) {   
-    checkWinnder();
     const computerSelection = computerChoice();
     playRound(playerSelection,computerSelection);
     updateChoices(playerSelection,computerSelection);
     updateScore();
+
+    if (checkWinner()) {
+        declareWinner();        
+    }
 }
 
 function computerChoice() {
@@ -34,21 +63,30 @@ function computerChoice() {
     }
 }
 
-function checkWinnder() {
-    if (playerScore === 5 || computerScore === 5) {
-        declareWinner();
-    }
+function checkWinner() {
+    return playerScore === 5 || computerScore === 5;
 }
 
 function declareWinner() {
     if (playerScore === 5) {
-        
+        document.getElementById("playerWin").style.display = "flex";
+    } 
+    if (computerScore === 5) {
+        document.getElementById("computerWin").style.display = "flex";
+        compVid();
     }
 }
 
+function compVid() {
+    let compVid = document.getElementById("compVid");
+    compVid.play();
+}
+
 function playRound(playerSelection,computerSelection) {    
+    checkWinner();
     if (playerSelection === computerSelection) {
         roundWinner = 'tie';
+        ++round;
     } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection = 'paper' && computerSelection === 'rock') ||
@@ -56,9 +94,11 @@ function playRound(playerSelection,computerSelection) {
     ) {
         roundWinner = 'player';
         ++playerScore;
+        ++round;
     } else {
         roundWinner = 'computer';
         ++computerScore;
+        ++round;
     } return roundWinner;
 }
 
@@ -98,7 +138,7 @@ function updateChoices(playerSelection, computerSelection) {
       if (roundWinner === 'computer') {
           scoreMes.textContent = 'Thua rồi! Cố nên pạn!';
       }
-
+      roundMes.textContent = `Ván: ${round}`;
       playerScoreMes.textContent = `Pạn: ${playerScore}`;
       computerScoreMes.textContent = `Vida: ${computerScore}`;
   }
